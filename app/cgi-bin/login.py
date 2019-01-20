ef8b55dce618aef788b67a6d4ec520f89b53e87f
OliverHaoLong <34762296+OliverHaoLong@users.noreply.github.com>
2019-01-20 19:57:27 +0800

    Update login.py
    test the simple situation

--- app/cgi-bin/login.py
+++ app/cgi-bin/login.py
@@ -2,8 +2,8 @@ import cgi
 form = cgi.FieldStorage()
 print('Content-type: text/html\n')
 print('<title>Reply Page</title>')
-if not 'user' in form:
+if 'user' = 'hao' :
     print('<h1>Please enter your name.</h1>')
 else:
-    print('<h1>Hello <i>%s</i>!</h1> %cgi.escape(form['user'].value))
+    print('<h1>Hello <i>%s</i>!</h1>')
     
